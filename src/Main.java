14f635e3c532b1c77f4299219921dd2ded586203
Professional <new99kpicem@gmail.com>
2023-03-04 13:20:28 +0300

    nooooo

--- src/Main.java
+++ src/Main.java
@@ -30,12 +30,25 @@ public class Main {
     public static void task2() {
         System.out.println("Задача 2");
         int[] arr = generateRandomArray();
+        int minElement, maxElement;
+        minElement = arr[0];
+        maxElement = arr[1];
+        boolean rule = maxElement > minElement;
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
-        Arrays.sort(arr);
-        System.out.println("Минимальная сумма трат за день составила " + arr[0] + " рублей. Максимальная сумма трат за день составила "+ arr[29] + " рублей");
+        System.out.println(rule);
+        for (int i : arr) {
+            if (rule == false) {
+                minElement = arr[1];
+                maxElement = arr[0];
+            }
+        }
+        System.out.println("максимальный = "+maxElement);
+        System.out.println("минимальный = "+minElement);
+        //Arrays.sort(arr);
+        //System.out.println("Минимальная сумма трат за день составила " + arr[0] + " рублей. Максимальная сумма трат за день составила "+ arr[29] + " рублей");
     }
     public static void task3() {
         System.out.println("Задача 3");
